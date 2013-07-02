dcd39cea365020eab6fafd895df2890864a9a5d6
Jaroslaw Lewandowski <jotel@jotel.net>
2013-07-02 22:45:07 +0100

    - Fixing shadows for text with stroke in iOS - Improved text shadows in Android (they look the same as in iOS)

--- cocos2dx/platform/ios/CCImage.mm
+++ cocos2dx/platform/ios/CCImage.mm
@@ -388,10 +388,14 @@ static bool _initWithString(const char * pText, cocos2d::CCImage::ETextAlign eAl
             textOrigingY = startH - shadowStrokePaddingY;
         }
         
+        CGRect rect = CGRectMake(textOriginX, textOrigingY, textWidth, textHeight);
         
+        CGContextBeginTransparencyLayerWithRect(context, rect, NULL);
         // actually draw the text in the context
 		// XXX: ios7 casting
-        [str drawInRect:CGRectMake(textOriginX, textOrigingY, textWidth, textHeight) withFont:font lineBreakMode:NSLineBreakByWordWrapping alignment:(NSTextAlignment)align];
+        [str drawInRect: rect withFont:font lineBreakMode:NSLineBreakByWordWrapping alignment:(NSTextAlignment)align];
+
+        CGContextEndTransparencyLayer(context);
         
         // pop the context
         UIGraphicsPopContext();
