cocos2d-cpp/cocos2d-cpp

Solving 'black screen' on android

cocos2dx/textures/CCTextureCache.cpp
@@ -646,14 +646,9 @@ VolatileTexture::VolatileTexture(Texture2D *t)
 , _pixelFormat(kTexture2DPixelFormat_RGBA8888)
 , _fileName("")
 , _fmtImage(Image::kFmtPng)
-, _alignment(kTextAlignmentCenter)
-, _vAlignment(kVerticalTextAlignmentCenter)
-, _fontName("")
 , _text("")
 , uiImage(NULL)
-, _fontSize(0.0f)
 {
-    _size = CCSizeMake(0, 0);
     _texParams.minFilter = GL_LINEAR;
     _texParams.magFilter = GL_LINEAR;
     _texParams.wrapS = GL_CLAMP_TO_EDGE;
@@ -727,8 +722,7 @@ void VolatileTexture::addDataTexture(Texture2D *tt, void* data, Texture2DPixelFo
     vt->_textureSize = contentSize;
 }
 
-void VolatileTexture::addStringTexture(Texture2D *tt, const char* text, const Size& dimensions, TextAlignment alignment, 
-                                       VerticalTextAlignment vAlignment, const char *fontName, float fontSize)
+void VolatileTexture::addStringTexture(Texture2D *tt, const char* text, const FontDefinition& fontDefinition)
 {
     if (isReloading)
     {
@@ -738,12 +732,8 @@ void VolatileTexture::addStringTexture(Texture2D *tt, const char* text, const Si
     VolatileTexture *vt = findVolotileTexture(tt);
 
     vt->_cashedImageType = kString;
-    vt->_size        = dimensions;
-    vt->_fontName = fontName;
-    vt->_alignment   = alignment;
-    vt->_vAlignment  = vAlignment;
-    vt->_fontSize   = fontSize;
     vt->_text     = text;
+    vt->_fontDefinition = fontDefinition;
 }
 
 void VolatileTexture::setTexParameters(Texture2D *t, const ccTexParams &texParams)
@@ -834,13 +824,7 @@ void VolatileTexture::reloadAllTextures()
             break;
         case kString:
             {
-                vt->texture->initWithString(vt->_text.c_str(),
-                                            vt->_fontName.c_str(),
-                                            vt->_fontSize,
-                                            vt->_size,
-                                            vt->_alignment,
-                                            vt->_vAlignment
-                                            );
+                vt->texture->initWithString(vt->_text.c_str(), vt->_fontDefinition);
             }
             break;
         case kImage:

cocos2dx/textures/CCTextureCache.h
@@ -213,8 +213,7 @@ public:
     ~VolatileTexture();
 
     static void addImageTexture(Texture2D *tt, const char* imageFileName, Image::EImageFormat format);
-    static void addStringTexture(Texture2D *tt, const char* text, const Size& dimensions, TextAlignment alignment, 
-                                 VerticalTextAlignment vAlignment, const char *fontName, float fontSize);
+    static void addStringTexture(Texture2D *tt, const char* text, const FontDefinition& fontDefinition);
     static void addDataTexture(Texture2D *tt, void* data, Texture2DPixelFormat pixelFormat, const Size& contentSize);
     static void addImage(Texture2D *tt, Image *image);
 
@@ -245,13 +244,9 @@ protected:
     std::string _fileName;
     Image::EImageFormat _fmtImage;
 
-    ccTexParams     _texParams;
-    Size          _size;
-    TextAlignment _alignment;
-    VerticalTextAlignment _vAlignment;
-    std::string     _fontName;
-    std::string     _text;
-    float           _fontSize;
+    ccTexParams      _texParams;
+    std::string      _text;
+    FontDefinition   _fontDefinition;
 };
 
 #endif

jotel/cocos2d-x

cocos2dx/textures/CCTextureCache.cpp
@@ -714,14 +714,9 @@ VolatileTexture::VolatileTexture(CCTexture2D *t)
 , m_PixelFormat(kTexture2DPixelFormat_RGBA8888)
 , m_strFileName("")
 , m_FmtImage(CCImage::kFmtPng)
-, m_alignment(kCCTextAlignmentCenter)
-, m_vAlignment(kCCVerticalTextAlignmentCenter)
-, m_strFontName("")
 , m_strText("")
 , uiImage(NULL)
-, m_fFontSize(0.0f)
 {
-    m_size = CCSizeMake(0, 0);
     m_texParams.minFilter = GL_LINEAR;
     m_texParams.magFilter = GL_LINEAR;
     m_texParams.wrapS = GL_CLAMP_TO_EDGE;
@@ -795,8 +790,7 @@ void VolatileTexture::addDataTexture(CCTexture2D *tt, void* data, CCTexture2DPix
     vt->m_TextureSize = contentSize;
 }
 
-void VolatileTexture::addStringTexture(CCTexture2D *tt, const char* text, const CCSize& dimensions, CCTextAlignment alignment, 
-                                       CCVerticalTextAlignment vAlignment, const char *fontName, float fontSize)
+void VolatileTexture::addStringTexture(CCTexture2D *tt, const char* text, const FontDefinition& fontDefinition)
 {
     if (isReloading)
     {
@@ -806,12 +800,8 @@ void VolatileTexture::addStringTexture(CCTexture2D *tt, const char* text, const 
     VolatileTexture *vt = findVolotileTexture(tt);
 
     vt->m_eCashedImageType = kString;
-    vt->m_size        = dimensions;
-    vt->m_strFontName = fontName;
-    vt->m_alignment   = alignment;
-    vt->m_vAlignment  = vAlignment;
-    vt->m_fFontSize   = fontSize;
     vt->m_strText     = text;
+    vt->m_fontDefinition = fontDefinition;
 }
 
 void VolatileTexture::setTexParameters(CCTexture2D *t, ccTexParams *texParams) 
@@ -902,13 +892,7 @@ void VolatileTexture::reloadAllTextures()
             break;
         case kString:
             {
-                vt->texture->initWithString(vt->m_strText.c_str(),
-                                            vt->m_strFontName.c_str(),
-                                            vt->m_fFontSize,
-                                            vt->m_size,
-                                            vt->m_alignment,
-                                            vt->m_vAlignment
-                                            );
+                vt->texture->initWithString(vt->m_strText.c_str(), vt->_fontDefinition);
             }
             break;
         case kImage:

cocos2dx/textures/CCTextureCache.h
@@ -200,8 +200,7 @@ public:
     ~VolatileTexture();
 
     static void addImageTexture(CCTexture2D *tt, const char* imageFileName, CCImage::EImageFormat format);
-    static void addStringTexture(CCTexture2D *tt, const char* text, const CCSize& dimensions, CCTextAlignment alignment, 
-                                 CCVerticalTextAlignment vAlignment, const char *fontName, float fontSize);
+    static void addStringTexture(CCTexture2D *tt, const char* text, const FontDefinition& fontDefinition);
     static void addDataTexture(CCTexture2D *tt, void* data, CCTexture2DPixelFormat pixelFormat, const CCSize& contentSize);
     static void addCCImage(CCTexture2D *tt, CCImage *image);
 
@@ -232,13 +231,9 @@ protected:
     std::string m_strFileName;
     CCImage::EImageFormat m_FmtImage;
 
-    ccTexParams     m_texParams;
-    CCSize          m_size;
-    CCTextAlignment m_alignment;
-    CCVerticalTextAlignment m_vAlignment;
-    std::string     m_strFontName;
-    std::string     m_strText;
-    float           m_fFontSize;
+    ccTexParams      m_texParams;
+    std::string      m_strText;
+    FontDefinition   m_fontDefinition;
 };
 
 #endif